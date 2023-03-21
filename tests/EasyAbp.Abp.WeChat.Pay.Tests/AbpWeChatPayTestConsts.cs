69cfb2f60500933f77c4ff513d5f6583cf11d5e7
real-zony <zony@myzony.com>
2023-03-21 19:13:13 +0800

    chore: Merged code from master branch.

--- tests/EasyAbp.Abp.WeChat.Pay.Tests/AbpWeChatPayTestConsts.cs
+++ tests/EasyAbp.Abp.WeChat.Pay.Tests/AbpWeChatPayTestConsts.cs
@@ -3,7 +3,7 @@
     public class AbpWeChatPayTestConsts
     {
         public const bool IsSandBox = false;
-        public const string MchId = "";
+        public const string MchId = "1540561391";
         public const string ApiKey = "";
         public const string AppId = "";
         public const string OpenId = "";
