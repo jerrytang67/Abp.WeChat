bd7a2657248dad22fcd8f4217dd2b5c6d43f0e65
real-zony <zony@myzony.com>
2023-03-21 19:59:32 +0800

    refactor: The payment service for version 2 has been removed.

---
+++ src/Pay/EasyAbp.Abp.WeChat.Pay/Services/BasicPayment/JsPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using EasyAbp.Abp.WeChat.Pay.Options;
+using Volo.Abp.DependencyInjection;
+
+namespace EasyAbp.Abp.WeChat.Pay.Services.BasicPayment;
+
+public class JsPaymentService : WeChatPayServiceBase
+{
+    public JsPaymentService(AbpWeChatPayOptions options, IAbpLazyServiceProvider lazyServiceProvider) : base(options, lazyServiceProvider)
+    {
+    }
+}
